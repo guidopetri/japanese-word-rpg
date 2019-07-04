b0f332737eea4142af89ac2b317056445574c3c1
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-07-04 22:15:31 +0200

    main story points

--- src/story.py
+++ src/story.py
@@ -3,21 +3,21 @@
 king_story = {1: ["welcome to edo. we are pleased to have you here. "
                   "monsters have been attacking us recently. "
                   "would you be so kind as to slay some for us?"],
-              2: ["it seems the monsters have only been getting stronger.",
-                  "there seems to be some sort of spell on a few of them...",
-                  "do you think you could bring back some more monster parts?",
+              2: ["it seems the monsters have only been getting stronger. "
+                  "there seems to be some sort of spell on a few of them... "
+                  "do you think you could bring back some more monster parts? "
                   "that way we can investigate some more."],
-              3: ["well, thank you for bringing those monster parts.",
-                  "unfortunately, the truth is as we suspected.",
-                  "the monsters have been spawning because of an evil guy.",
-                  "if you find some more monsters, maybe you can tell...",
-                  "... where they're coming from."],
-              4: ["you found the evil guy's dungeon!",
-                  "please defeat him and bring peace back to edo.",
+              3: ["well, thank you for bringing those monster parts. "
+                  "unfortunately, the truth is as we suspected. "
+                  "the monsters have been spawning because of an evil guy. "
+                  "if you find some more monsters, maybe you can tell "
+                  "where they're coming from."],
+              4: ["you found the evil guy's dungeon! "
+                  "please defeat him and bring peace back to edo. "
                   "we are counting on you."],
-              5: ["peace is restored!",
-                  "thank you so much for your hard work.",
-                  "we will forever be in your debt, hero.",
+              5: ["peace is restored! "
+                  "thank you so much for your hard work. "
+                  "we will forever be in your debt, hero. "
                   "please accept this token of our thanks."]}
 
 king_items_given = {1: [],
---
+++ story-notes/overview.md
@@ -0,0 +1,7 @@
+# main storyline
+
+- monsters appeared around town
+- evil guy has been spawning them
+- monster parts reveal the evil guy was spawning them
+- evil guy's dungeon is revealed
+- once evil guy is defeated, hero receives praise
