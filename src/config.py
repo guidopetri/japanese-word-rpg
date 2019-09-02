f6ef1cbb2fc8a38b9835eb9f2889c6f276098fa9
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-09-02 14:39:40 -0400

    different directory for linux save data

--- src/config.py
+++ src/config.py
@@ -1,10 +1,17 @@
 #! /usr/bin/env python
 
 from os.path import expanduser
+from sys import platform as _platform
 
 fontname = 'Arial'
 fontsize = 32
 width = 800
 height = 600
 player = None
-savepath = expanduser('~/AppData/Roaming/Edo/')
+
+if _platform == 'linux':
+    savepath = '~/Edo/'
+elif _platform == 'win32':
+    savepath = '~/AppData/Roaming/Edo/'
+
+savepath = expanduser(savepath)
